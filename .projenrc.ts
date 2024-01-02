a9a27e81bef100409eaf3ff7fb3eb4157c0abd18
mrpackethead <andrew.frazer@raindancers.cloud>
2024-01-02 02:57:04 +0000

    feat: new deps

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,32 @@
 import { cdk } from 'projen';
 const project = new cdk.JsiiProject({
-  author: 'mrpackethead',
+  author: 'MrpAcketheAd',
   authorAddress: 'andrew.frazer@raindancers.cloud',
+  authorOrganization: true,
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.0.0',
-  name: 'ts-cdk-pipeline-app',
+  name: 'projen-pipelines',
   projenrcTs: true,
-  repositoryUrl: 'https://git.us-west-2.github.source.3p.codecatalyst.aws/v1/DOC/ts-cdk-pipeline-app/ts-cdk-pipeline-app',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  repositoryUrl: 'https://github.com/tepapaatawhai/ts-cdk-pipeline-apps.git',
+  licensed: true,
+  license: 'Apache-2.0',
+  devDeps: [
+    'constructs',
+  ],
+  deps: [
+    'projen',
+  ],
+  peerDeps: [
+    'projen',
+    'constructs',
+  ],
+  keywords: [
+    'aws',
+    'cdk',
+  ],
+  tsconfig: {
+    compilerOptions: {
+      esModuleInterop: true,
+    },
+  },
 });
 project.synth();
